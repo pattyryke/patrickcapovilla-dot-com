d0b8958aac78342b76a3e3b46d625a34570eedf0
Patrick Capovilla <patjoecap@gmail.com>
2024-05-16 15:37:18 -0700

    Deleted all files.

--- app/default.tsx
+++
@@ -1,7 +0,0 @@
-
-
-
-
-export default function Default() {
-    return <h1>Default Page</h1>
-}
--- ui/DividerComponent.jsx
+++
@@ -1,3 +0,0 @@
-export default function DividerComponent() {
-    return <span className='block h-0.5 w-full my-3 bg-accent bg-opacity-30' />;
-}
