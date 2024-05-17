0e8202dfb4116c45daffe8b7188958d7640b1fae
Pattyryke <patjoecap@gmail.com>
2024-05-16 21:48:41 -0700

    Small adjustments to config to make this stupid app work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/pattyryke/patrickcapovilla-dot-com',  
+  base: 'https://www.patrickcapovilla.com',  
   plugins: [react()],
 })
